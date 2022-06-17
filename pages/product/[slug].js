9cab86df2bfd6b6bb572de92ff638feae9ad2665
Neil Rowland <neilrowland56@yahoo.com>
2022-06-17 07:33:39 +0100

    Upto Payment Method

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -62,7 +62,7 @@ export default function ProductScreen() {
           <div className="card p-5">
             <div className="mb-2 flex justify-between">
               <div>Price</div>
-              <div>${product.price}</div>
+              <div>£{product.price}</div>
             </div>
             <div className="mb-2 flex justify-between">
               <div>Status</div>
